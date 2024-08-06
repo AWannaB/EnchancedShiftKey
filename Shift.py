754d8889b96b9fe8a23070c98c4ec2a23ba9e21a
AWannaB <theocarruth+github@gmail.com>
2024-08-06 16:49:21 -0700

    Bug Fix
    Fixes some bugs

--- Shift.py
+++ Shift.py
@@ -1,4 +1,5 @@
 import keyboard
+import time
 # Starts an infinite loop
 while True:
     # The program does nothing until the Shift key is pressed
@@ -9,7 +10,16 @@ while True:
     key1 = keyboard.read_key()
     # Capitalizes the key if it is an alphabetic key and does nothing if it is not
     key2 = key1.upper() if key1.isalpha() else key1
-    # Deletes the lower case letter
-    keyboard.press_and_release('backspace')
-    # Writes the capitalized letter
-    keyboard.write(key2)
+    # Stops from printing the following keys
+    if key2 == key1 or key2 == 'SHIFT' or key2 == 'RIGHT SHIFT' or key2 == 'ENTER':
+        continue
+    elif key2 == 'BACKSPACE' or key2 == 'TAB' or key2 == 'CTRL' or key2 == 'SPACE' or key2 == 'ALT':
+        continue
+    else:
+        # Deletes the lower case letter
+        keyboard.press_and_release('backspace')
+        '''Fixes bug on lower-end systems form interpreting the 
+        backspace and writeing the capitalized letter in the wrong order'''
+        time.sleep(0.05)
+        # Writes the capitalized letter
+        keyboard.write(key2)

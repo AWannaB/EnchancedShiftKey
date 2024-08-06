ca0918f69aa5e4a34d37fa8a48414e1abb6a1e16
AWannaB <theocarruth+github@gmail.com>
2024-08-06 16:38:14 -0700

    added comments explaining the code

--- Shift.py
+++ Shift.py
@@ -1,8 +1,15 @@
 import keyboard
+# Starts an infinite loop
 while True:
+    # The program does nothing until the Shift key is pressed
     keyboard.wait('shift')
+    # Reads the shift key (this is the fastest way to solve the issue of the code capitalizing the shift key and having no effect)
     key0 = keyboard.read_key()
+    # Reads the key that needs to be capitalized
     key1 = keyboard.read_key()
+    # Capitalizes the key if it is an alphabetic key and does nothing if it is not
     key2 = key1.upper() if key1.isalpha() else key1
+    # Deletes the lower case letter
     keyboard.press_and_release('backspace')
+    # Writes the capitalized letter
     keyboard.write(key2)
